de360d80f0f53994e55e02e9daa96e81b9731909
zoyachann <zoyachanna644@gmail.com>
2024-12-13 13:43:53 +0500

    add navbar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import ResponsiveAppBar from './components/header';
+
+function App() {
+  return (
+   
+   <div>
+    <ResponsiveAppBar/>
+   
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Container from '@mui/material/Container';
+import Logo from '../asess/Logo-new.webp';
+import '@fontsource/roboto/400.css';
+import AppleIcon from '@mui/icons-material/Apple';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const ResponsiveAppBar = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+
+        <AppBar position="sticky" color="default" elevation={0} justify-content-between>
+
+            <Toolbar>
+                {/* Logo */}
+                <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1, marginLeft: '20' }}>
+                    <img src={Logo} alt="PickBazar Logo" style={{ height: 40, marginRight: 8 }} />
+                    <Typography
+
+                        variant="h6"
+                        component="div"
+                        sx={{ fontWeight: 'bold', color: '#333', marginRight: '20px' }}
+                    >
+                        PickBazar
+                    </Typography>
+                    <Box className='d-flex  border border-success' >
+                        <Button variant='outlined' sx={{ color: '#019376', border:'1px solid #019376'}}startIcon={<AppleIcon />} endIcon={<ArrowDropDownIcon />}  >
+                            Grocery
+                        </Button>
+
+
+                    </Box> 
+                    
+                </Box>
+
+
+                {/* Links */}
+                <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 2 }}>
+                    <Button color="inherit">Shops</Button>
+                    <Button color="inherit">Offers</Button>
+                    <Button color="inherit">Contact</Button>
+                    <Button
+                        color="inherit"
+                        endIcon={<ExpandMoreIcon  />}
+                        onClick={handleMenuOpen}
+                 sx={{marginRight:'10px'}}
+                    >
+                        Pages
+                    </Button>
+                    <Menu
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={handleMenuClose}
+                        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+                        transformOrigin={{ vertical: 'top', horizontal: 'left' }}
+                    >
+                        <MenuItem onClick={handleMenuClose}>Page 1</MenuItem>
+                        <MenuItem onClick={handleMenuClose}>Page 2</MenuItem>
+                        <MenuItem onClick={handleMenuClose}>Page 3</MenuItem>
+                    </Menu>
+                </Box>
+
+                {/* Buttons */}
+                <Box sx={{ display: 'flex', gap: 1 }}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        sx={{ textTransform: 'none', background: '#019376' }}
+                    >
+                        Join
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="success"
+                        sx={{ textTransform: 'none', background: '#019376' }}
+                    >
+                        Become a Seller
+                    </Button>
+                </Box>
+            </Toolbar>
+
+        </AppBar>
+
+    );
+};
+
+export default ResponsiveAppBar;
+
